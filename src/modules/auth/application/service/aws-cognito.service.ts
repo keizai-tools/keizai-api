834ebce75ae2c3e0210999b7a8287bfcc6be74e4
github_fedeesti <fede.estigarribia@hotmail.com>
2023-10-09 19:06:53 -0300

    Add AWS Cognito Service

---
+++ src/modules/auth/application/service/aws-cognito.service.ts
@@ -0,0 +1,63 @@
+import { Injectable } from '@nestjs/common';
+import {
+  AuthenticationDetails,
+  CognitoUser,
+  CognitoUserPool,
+} from 'amazon-cognito-identity-js';
+
+import { AuthRequestDto } from '../../interface/dto/auth-request.to.ts';
+
+@Injectable()
+export class AwsCognitoService {
+  private userPool: CognitoUserPool;
+
+  constructor() {
+    this.userPool = new CognitoUserPool({
+      UserPoolId: process.env.AWS_COGNITO_USER_POOL_ID,
+      ClientId: process.env.AWS_COGNITO_CLIENT_ID,
+    });
+  }
+
+  async registerUser(registerRequestDto: AuthRequestDto) {
+    const { email, password } = registerRequestDto;
+
+    return new Promise((resolve, reject) => {
+      this.userPool.signUp(email, password, null, null, (err, result) => {
+        if (!result) {
+          reject(err);
+        } else {
+          resolve(result.user);
+        }
+      });
+    });
+  }
+
+  async authenticateUser(authLoginUserDto: AuthRequestDto) {
+    const { email, password } = authLoginUserDto;
+    const userData = {
+      Username: email,
+      Pool: this.userPool,
+    };
+
+    const authenticationDetails = new AuthenticationDetails({
+      Username: email,
+      Password: password,
+    });
+
+    const userCognito = new CognitoUser(userData);
+
+    return new Promise((resolve, reject) => {
+      userCognito.authenticateUser(authenticationDetails, {
+        onSuccess: (result) => {
+          resolve({
+            accessToken: result.getAccessToken().getJwtToken(),
+            refreshToken: result.getRefreshToken().getToken(),
+          });
+        },
+        onFailure: (err) => {
+          reject(err);
+        },
+      });
+    });
+  }
+}
