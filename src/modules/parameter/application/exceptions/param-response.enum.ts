b82faa547a4e5ec59de280f8895fd2f5625b2d67
Fede Estigarribia <fede.estigarribia@hotmail.com>
2023-10-18 18:21:03 -0300

    Add Service to params

---
+++ src/modules/parameter/application/exceptions/param-response.enum.ts
@@ -0,0 +1,9 @@
+export enum PARAM_RESPONSE {
+  PARAM_NOT_FOUND = 'Parameter not found',
+  PARAM_NOT_FOUND_BY_ID = 'Parameter not found by id',
+  PARAM_NOT_FOUND_BY_USER = 'Parameter not found by user',
+  PARAM_NOT_FOUND_BY_USER_AND_ID = 'Parameter not found by user and id',
+  PARAM_NOT_DELETED = 'Parameter not deleted',
+  PARAM_NOT_UPDATED = 'Parameter not updated',
+  PARAM_INVOCATION_NOT_FOUND = 'Invocation not exists',
+}
