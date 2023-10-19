4bd7df3b804a7b10f072c81eebdb6afd42bf098b
Leonel Tammaro <tammaroleonel@gmail.com>
2023-10-19 20:21:49 -0300

    fix method response enum

--- src/modules/method/application/exceptions/method-response.enum.ts
+++ src/modules/method/application/exceptions/method-response.enum.ts
@@ -1,9 +1,9 @@
 export enum METHOD_RESPONSE {
-  METHOD_NOT_FOUND = 'METHOD not found',
-  METHOD_NOT_FOUND_BY_ID = 'METHOD not found by id',
-  METHOD_NOT_FOUND_BY_USER = 'METHOD not found by user',
-  METHOD_NOT_FOUND_BY_USER_AND_ID = 'METHOD not found by user and id',
-  METHOD_NOT_DELETED = 'METHOD not deleted',
-  METHOD_NOT_UPDATED = 'METHOD not updated',
+  METHOD_NOT_FOUND = 'Method not found',
+  METHOD_NOT_FOUND_BY_ID = 'Method not found by id',
+  METHOD_NOT_FOUND_BY_USER = 'Method not found by user',
+  METHOD_NOT_FOUND_BY_USER_AND_ID = 'Method not found by user and id',
+  METHOD_NOT_DELETED = 'Method not deleted',
+  METHOD_NOT_UPDATED = 'Method not updated',
   METHOD_INVOCATION_NOT_FOUND = 'Invocation not exists',
 }
