907a91acd37642f90fdf1422240fe5c0c48451a8
Fede Estigarribia <fede.estigarribia@hotmail.com>
2023-10-20 13:55:11 -0300

    Add error handler in invocation module

--- src/modules/invocation/application/exceptions/invocation-response.enum.dto.ts
+++ src/modules/invocation/application/exceptions/invocation-response.enum.dto.ts
@@ -3,6 +3,7 @@ export enum INVOCATION_RESPONSE {
   Invocation_NOT_FOUND_BY_ID = 'Invocation not found by id',
   Invocation_NOT_FOUND_BY_USER = 'Invocation not found by user',
   Invocation_NOT_FOUND_BY_USER_AND_ID = 'Invocation not found by user and id',
+  Invocation_NOT_SAVE = 'Invocation not saved',
   Invocation_NOT_DELETED = 'Invocation not deleted',
   Invocation_NOT_UPDATED = 'Invocation not updated',
   INVOCATION_FOLDER_NOT_EXISTS = 'Folder not exists or its not yours',
