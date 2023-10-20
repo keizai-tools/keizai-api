98c003c3a50ad64eb3f14c98e60c5791c15ccd3d
Leonel Tammaro <tammaroleonel@gmail.com>
2023-10-20 00:16:34 -0300

    add migration to update invocation table

--- data/migrations/1697771713813-update-to-nullable-invocation-table.ts
+++ data/migrations/1697771713813-update-to-nullable-invocation-table.ts
@@ -1,14 +1,16 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class UpdateInvocationTable1697769332179 implements MigrationInterface {
-  name = 'UpdateInvocationTable1697769332179';
+export class UpdateToNullableInvocationTable1697771713813
+  implements MigrationInterface
+{
+  name = 'UpdateToNullableInvocationTable1697771713813';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE \`invocation\` ADD \`secret_key\` varchar(255) NULL`,
+      `ALTER TABLE \`invocation\` CHANGE \`secret_key\` \`secret_key\` varchar(255) NULL`,
     );
     await queryRunner.query(
-      `ALTER TABLE \`invocation\` ADD \`public_key\` varchar(255) NULL`,
+      `ALTER TABLE \`invocation\` CHANGE \`public_key\` \`public_key\` varchar(255) NULL`,
     );
     await queryRunner.query(
       `ALTER TABLE \`invocation\` CHANGE \`contract_id\` \`contract_id\` varchar(255) NULL`,
@@ -20,10 +22,10 @@ export class UpdateInvocationTable1697769332179 implements MigrationInterface {
       `ALTER TABLE \`invocation\` CHANGE \`contract_id\` \`contract_id\` varchar(255) NOT NULL`,
     );
     await queryRunner.query(
-      `ALTER TABLE \`invocation\` DROP COLUMN \`public_key\``,
+      `ALTER TABLE \`invocation\` CHANGE \`public_key\` \`public_key\` varchar(255) NOT NULL`,
     );
     await queryRunner.query(
-      `ALTER TABLE \`invocation\` DROP COLUMN \`secret_key\``,
+      `ALTER TABLE \`invocation\` CHANGE \`secret_key\` \`secret_key\` varchar(255) NOT NULL`,
     );
   }
 }
