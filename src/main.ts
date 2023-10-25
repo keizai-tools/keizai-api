e608b96417c5178a604a8287bbd7b5dfbd704461
Ivan Tammaro <tammaroivan@gmail.com>
2023-10-26 08:11:16 +1100

    Implement swagger

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,24 @@
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory, Reflector } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+
+  const config = new DocumentBuilder()
+    .setTitle('Keizai')
+    .setDescription('Keizai API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config, {
+    deepScanRoutes: true,
+  });
+  SwaggerModule.setup('api', app, document);
+
   app.setGlobalPrefix('api');
   app.enableCors();
 
