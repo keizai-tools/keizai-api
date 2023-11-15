52ade0865a9e4cfd3534027b259912c5fa18585b
Fede Estigarribia <fede.estigarribia@hotmail.com>
2023-11-15 14:52:02 -0300

    Add enviroment enum and mapper

---
+++ src/modules/enviroment/application/exceptions/enviroment-response.enum.ts
@@ -0,0 +1,8 @@
+export enum ENVIROMENT_RESPONSE {
+  ENVIROMENT_NOT_FOUND_BY_COLLECTION_AND_USER = 'The user of the collection and the logged in user do not match',
+  ENVIROMENT_NOT_FOUND_BY_USER_ID = 'The user id does not match',
+  ENVIROMENT_COLLECTION_NOT_FOUND = 'The collection does not exist',
+  ENVIROMENT_FAILED_SAVE = 'Could not save variable, please try again',
+  ENVIROMENT_FAILED_DELETED = 'Could not deleted variable, please try again',
+  ENVIROMENT_FAILED_UPDATED = 'Could not updated variable, please try again',
+}
---
+++ src/modules/enviroment/application/mapper/enviroment.mapper.ts
@@ -0,0 +1,25 @@
+import { Enviroment } from '../../domain/enviroment.domain';
+import { EnviromentResponseDto } from '../dto/enviroment-response.dto';
+import {
+  IEnviromentValues,
+  IUpdateEnviromentValues,
+} from '../service/enviroment.service';
+
+export class EnviromentMapper {
+  fromDtoToEntity(createEnviromentDto: IEnviromentValues): Enviroment {
+    const { name, type, value, collectionId, userId } = createEnviromentDto;
+    return new Enviroment(name, type, value, collectionId, userId);
+  }
+
+  fromEntityToDto(enviroment: Enviroment): EnviromentResponseDto {
+    const { name, type, value, id } = enviroment;
+    return new EnviromentResponseDto(name, type, value, id);
+  }
+
+  fromUpdateDtoToEntity(
+    updateEnviromentDto: IUpdateEnviromentValues,
+  ): Enviroment {
+    const { name, type, value, collectionId, userId, id } = updateEnviromentDto;
+    return new Enviroment(name, type, value, collectionId, userId, id);
+  }
+}
