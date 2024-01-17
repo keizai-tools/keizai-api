f8cc72c70c8d811d24ea6b92bbf5543207f02774
Fede Estigarribia <fede.estigarribia@hotmail.com>
2024-01-17 17:05:10 -0300

    Add fromUpdateDtoToInvocationValues mapper

--- src/modules/invocation/application/mapper/invocation.mapper.ts
+++ src/modules/invocation/application/mapper/invocation.mapper.ts
@@ -5,6 +5,7 @@ import { MethodMapper } from '@/modules/method/application/mapper/method.mapper'
 
 import { Invocation } from '../../domain/invocation.domain';
 import { InvocationResponseDto } from '../dto/invocation-response.dto';
+import { UpdateInvocationDto } from '../dto/update-invocation.dto';
 import {
   IInvocationValues,
   IUpdateInvocationValues,
@@ -111,5 +112,35 @@ export class InvocationMapper {
       selectedMethodId,
       id,
     );
+  }
+  fromUpdateDtoToInvocationValues(
+    updateInvocationDto: UpdateInvocationDto,
+    userId: string,
+  ): IUpdateInvocationValues {
+    const {
+      name,
+      secretKey,
+      publicKey,
+      preInvocation,
+      postInvocation,
+      contractId,
+      network,
+      folderId,
+      selectedMethodId,
+      id,
+    } = updateInvocationDto;
+    return {
+      name,
+      secretKey: network ? null : secretKey,
+      publicKey: network ? null : publicKey,
+      preInvocation: network ? null : preInvocation,
+      postInvocation: network ? null : postInvocation,
+      contractId: network ? null : contractId,
+      network,
+      folderId,
+      selectedMethodId,
+      userId,
+      id,
+    };
   }
 }
