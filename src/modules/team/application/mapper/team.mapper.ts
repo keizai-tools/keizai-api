dc9a6336d574416e7258b64062320963b70d0c7c
Fede Estigarribia <fede.estigarribia@hotmail.com>
2024-02-19 12:07:36 -0300

    Add team service

---
+++ src/modules/team/application/mapper/team.mapper.ts
@@ -0,0 +1,34 @@
+import { Inject } from '@nestjs/common';
+
+import { CollectionMapper } from '@/modules/collection/application/mapper/collection.mapper';
+
+import { Team } from '../../domain/team.domain';
+import { TeamResponseDto } from '../dto/response-team.dto';
+import { ITeamData, IUpdateTeamData } from '../service/team.service';
+
+export class TeamMapper {
+  constructor(
+    @Inject(CollectionMapper)
+    private readonly collectionMapper: CollectionMapper,
+  ) {}
+
+  fromDtoToEntity(teamData: ITeamData): Team {
+    const { name, adminId, users } = teamData;
+    return new Team(name, adminId, users);
+  }
+
+  fromUpdateDtoToEntity(teamData: IUpdateTeamData): Team {
+    const { name, id, adminId, users } = teamData;
+    return new Team(name, adminId, users, id);
+  }
+
+  fromEntityToDto(team: Team): TeamResponseDto {
+    const { name, adminId, id, users, collections } = team;
+
+    const collectionsMapped = collections?.map((collection) => {
+      return this.collectionMapper.fromEntityToDto(collection);
+    });
+
+    return new TeamResponseDto(name, adminId, id, users, collectionsMapped);
+  }
+}
