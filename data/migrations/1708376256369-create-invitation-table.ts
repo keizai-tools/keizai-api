ee7926ad1be8d27728d80572e1c0d5439f1f564c
Leonel Tammaro <tammaroleonel@gmail.com>
2024-02-26 16:15:54 -0300

    create migrations to create invitation table and create teams

--- data/migrations/1708376256369-create-invitation-table.ts
+++ data/migrations/1708376256369-create-invitation-table.ts
@@ -8,9 +8,6 @@ export class CreateInvitationTable1708376256369 implements MigrationInterface {
       `CREATE TABLE \`invitation\` (\`id\` varchar(36) NOT NULL, \`created_at\` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updated_at\` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), \`team_id\` varchar(255) NOT NULL, \`from_user_id\` varchar(255) NOT NULL, \`to_user_id\` varchar(255) NOT NULL, \`status\` varchar(255) NOT NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`,
     );
     await queryRunner.query(
-      `ALTER TABLE \`enviroment\` ADD UNIQUE INDEX \`IDX_559fc3801cf1e98ca7b9be88cc\` (\`name\`)`,
-    );
-    await queryRunner.query(
       `ALTER TABLE \`invitation\` ADD CONSTRAINT \`FK_38e55bcd5e3166660d4bb827365\` FOREIGN KEY (\`from_user_id\`) REFERENCES \`user\`(\`id\`) ON DELETE CASCADE ON UPDATE NO ACTION`,
     );
     await queryRunner.query(
