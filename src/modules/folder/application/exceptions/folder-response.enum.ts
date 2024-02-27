b0fed270465ba15fa4ee004da0220fd2f69a1f9c
Fede Estigarribia <fede.estigarribia@hotmail.com>
2024-02-27 07:54:16 -0300

    Add folder/s not found in exceptions

--- src/modules/folder/application/exceptions/folder-response.enum.ts
+++ src/modules/folder/application/exceptions/folder-response.enum.ts
@@ -1,5 +1,7 @@
 export enum FOLDER_RESPONSE {
-  FOLDER_NOT_FOUND_BY_COLLECTION_AND_USER = 'The user of the collection and the logged in user do not match',
+  FOLDER_NOT_FOUND = 'Folder not found',
+  FOLDERS_NOT_FOUND = 'Folders not found',
+  FOLDER_NOT_FOUND_BY_COLLECTION_ID = 'The user of the collection and the logged in user do not match',
   FOLDER_NOT_FOUND_BY_USER_ID = 'The user id does not match',
   FOLDER_COLLECTION_NOT_FOUND = 'The collection does not exist',
   FOLDER_FAILED_SAVE = 'Could not save folder, please try again',
