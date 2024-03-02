be5ebda394d572ab6db466f50d854f4167a31611
Fede Estigarribia <fede.estigarribia@hotmail.com>
2024-03-02 09:49:30 -0300

    Add domain and schema

---
+++ src/modules/role/infrastructure/persistence/role.schema.ts
@@ -0,0 +1,35 @@
+import { EntitySchema } from 'typeorm';
+
+import { baseColumnSchemas } from '@/common/infrastructure/persistence/base.schema';
+
+import { UserRoleToTeam } from '../../domain/role.domain';
+
+export const UserRoleToTeamSchema = new EntitySchema<UserRoleToTeam>({
+  name: 'UserRoleToTeam',
+  target: UserRoleToTeam,
+  tableName: 'user_role_team',
+  columns: {
+    ...baseColumnSchemas,
+    role: {
+      type: 'varchar',
+    },
+    userId: {
+      type: 'varchar',
+    },
+    teamId: {
+      type: 'varchar',
+    },
+  },
+  relations: {
+    user: {
+      type: 'many-to-one',
+      target: 'User',
+      inverseSide: 'userRoleToTeam',
+    },
+    team: {
+      type: 'many-to-one',
+      target: 'Team',
+      inverseSide: 'userRoleToTeam',
+    },
+  },
+});
