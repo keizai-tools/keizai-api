f4caca2f6025be6a9045d110270d0800ea0007ab
Fede Estigarribia <fede.estigarribia@hotmail.com>
2024-03-04 11:00:28 -0300

    Add role relation in User

--- src/modules/auth/domain/user.domain.ts
+++ src/modules/auth/domain/user.domain.ts
@@ -1,15 +1,15 @@
 import { Base } from '@/common/domain/base.domain';
 import { Collection } from '@/modules/collection/domain/collection.domain';
 import { Folder } from '@/modules/folder/domain/folder.domain';
-import { Team } from '@/modules/team/domain/team.domain';
 import { Invitation } from '@/modules/invitation/domain/invitation.domain';
+import { UserRoleToTeam } from '@/modules/role/domain/role.domain';
 
 export class User extends Base {
   email: string;
   externalId: string;
   collections?: Collection[];
   folders?: Folder[];
-  teams?: Team[];
+  memberTeams?: UserRoleToTeam[];
   invitationsReceived?: Invitation[];
 
   constructor(email: string, externalId: string) {
