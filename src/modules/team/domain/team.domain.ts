2ef163e3f41ecdbda09472090a2c584ff5ed4ae9
Fede Estigarribia <fede.estigarribia@hotmail.com>
2024-03-04 11:01:18 -0300

    Add role relation in Team

--- src/modules/team/domain/team.domain.ts
+++ src/modules/team/domain/team.domain.ts
@@ -1,20 +1,19 @@
 import { Base } from '@/common/domain/base.domain';
-import { User } from '@/modules/auth/domain/user.domain';
 import { Collection } from '@/modules/collection/domain/collection.domain';
 import { Invitation } from '@/modules/invitation/domain/invitation.domain';
+import { UserRoleToTeam } from '@/modules/role/domain/role.domain';
 
 export class Team extends Base {
   id?: string;
   name: string;
   adminId: string;
-  users: User[];
+  userMembers: UserRoleToTeam[];
   invitations?: Invitation[];
   collections?: Collection[];
-  constructor(name: string, adminId: string, users: User[], id?: string) {
+  constructor(name: string, adminId: string, id?: string) {
     super();
     this.name = name;
     this.adminId = adminId;
-    this.users = users;
     this.id = id;
   }
 }
