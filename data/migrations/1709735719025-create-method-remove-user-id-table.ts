2d0600e5497af91c9db1e3afbf0c1f9011283d6e
Fede Estigarribia <fede.estigarribia@hotmail.com>
2024-03-06 11:36:16 -0300

    Remove migration with invalid query and add new migration

--- data/migrations/1709735719025-create-method-remove-user-id-table.ts
+++ data/migrations/1709735719025-create-method-remove-user-id-table.ts
@@ -1,22 +1,18 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class MethodsRemoveUserId1709047371166 implements MigrationInterface {
-  name = 'MethodsRemoveUserId1709047371166';
+export class CreateMethodRemoveUserIdTable1709735719025
+  implements MigrationInterface
+{
+  name = 'CreateMethodRemoveUserIdTable1709735719025';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
       `ALTER TABLE \`method\` DROP FOREIGN KEY \`FK_c9cc41154cb5ecfd0edcb3b16f4\``,
     );
     await queryRunner.query(`ALTER TABLE \`method\` DROP COLUMN \`user_id\``);
-    await queryRunner.query(
-      `ALTER TABLE \`invitation\` ADD CONSTRAINT \`FK_e252ae8906b8e70c59c65e4dcbe\` FOREIGN KEY (\`team_id\`) REFERENCES \`team\`(\`id\`) ON DELETE CASCADE ON UPDATE NO ACTION`,
-    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      `ALTER TABLE \`invitation\` DROP FOREIGN KEY \`FK_e252ae8906b8e70c59c65e4dcbe\``,
-    );
     await queryRunner.query(
       `ALTER TABLE \`method\` ADD \`user_id\` varchar(255) NOT NULL`,
     );
