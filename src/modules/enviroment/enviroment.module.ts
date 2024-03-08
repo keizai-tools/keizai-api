b4676a454bfe0dbfda1ac7fe34efa4064b175a4d
Fede Estigarribia <fede.estigarribia@hotmail.com>
2024-03-08 16:50:41 -0300

    Add environment.controller by team or user

--- src/modules/enviroment/enviroment.module.ts
+++ src/modules/enviroment/enviroment.module.ts
@@ -2,19 +2,22 @@ import { Module, forwardRef } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { CollectionModule } from '../collection/collection.module';
+import { TeamModule } from '../team/team.module';
 import { EnviromentMapper } from './application/mapper/enviroment.mapper';
 import { ENVIROMENT_REPOSITORY } from './application/repository/enviroment.repository';
 import { EnviromentService } from './application/service/enviroment.service';
 import { EnviromentSchema } from './infrastructure/persistence/enviroment.schema';
 import { EnviromentRepository } from './infrastructure/persistence/enviroment.typeorm.repository';
-import { EnviromentController } from './interface/enviroment.controller';
+import { EnviromentUserController } from './interface/enviroment.controller';
+import { EnviromentTeamController } from './interface/environment-team.controller';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([EnviromentSchema]),
     forwardRef(() => CollectionModule),
+    TeamModule,
   ],
-  controllers: [EnviromentController],
+  controllers: [EnviromentUserController, EnviromentTeamController],
   providers: [
     EnviromentService,
     EnviromentMapper,
