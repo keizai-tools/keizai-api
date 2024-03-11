91adaaf59d6dda8fbce871ff56406585bb771d5a
Fede Estigarribia <fede.estigarribia@hotmail.com>
2024-03-11 11:25:58 -0300

    Fix findAllByInvocationId in method.repository

--- src/modules/method/application/repository/method.interface.repository.ts
+++ src/modules/method/application/repository/method.interface.repository.ts
@@ -7,10 +7,7 @@ import { Method } from '../../domain/method.domain';
 export interface IMethodRepository extends IBaseRepository<Method> {
   saveAll(methods: Method[]): Promise<Method[]>;
   findAll(userId: string): Promise<Method[]>;
-  findAllByInvocationId(
-    invocationId: string,
-    userId: string,
-  ): Promise<Method[]>;
+  findAllByInvocationId(invocationId: string): Promise<Method[]>;
   findOneByIds(id: string, userId: string): Promise<Method>;
   update(param: Method): Promise<Method>;
   delete(id: string): Promise<boolean>;
