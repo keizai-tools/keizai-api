f279031f340d505c4a84eaf1d56deab431c78b3a
Fede Estigarribia <fede.estigarribia@hotmail.com>
2024-03-28 16:43:41 -0300

    Add tests to generateMethodsFromContractId

--- src/common/application/service/__test__/stellar.service.spec.ts
+++ src/common/application/service/__test__/stellar.service.spec.ts
@@ -1,10 +1,19 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Test, TestingModule } from '@nestjs/testing';
+import { xdr } from 'stellar-sdk';
 
 import { MethodMapper } from '@/modules/method/application/mapper/method.mapper';
 
 import { StellarMapper } from '../../mapper/contract.mapper';
-import { NETWORK } from '../../types/soroban.enum';
+import { CONTRACT_EXECUTABLE_TYPE, NETWORK } from '../../types/soroban.enum';
 import { StellarService } from '../stellar.service';
+
+const contractExecutable: xdr.ContractExecutable = {
+  switch: jest.fn(),
+  wasmHash: jest.fn(),
+  value: jest.fn(),
+  toXDR: jest.fn(),
+};
 
 describe('StellarService', () => {
   let service: StellarService;
@@ -39,6 +48,52 @@ describe('StellarService', () => {
 
       expect(changeNetworkSpy).toHaveBeenCalledWith(selectedNetwork);
       expect(changeNetworkSpy).toHaveBeenCalledTimes(1);
+    });
+  });
+  describe('generateMethodsFromContractId', () => {
+    it('Should return the stellar asset contract functions', async () => {
+      jest
+        .spyOn(service, 'getInstanceValue')
+        .mockResolvedValue(contractExecutable);
+      jest.spyOn(contractExecutable, 'switch').mockReturnValue({
+        name: CONTRACT_EXECUTABLE_TYPE.STELLAR_ASSET,
+        value: 1,
+      });
+      jest
+        .spyOn(service, 'getStellarAssetContractFunctions')
+        .mockReturnValue([]);
+      jest.spyOn(service, 'getContractSpecEntries');
+
+      const result = await service.generateMethodsFromContractId('contractId');
+
+      expect(service.getInstanceValue).toHaveBeenCalled();
+      expect(service.getStellarAssetContractFunctions).toHaveBeenCalled();
+      expect(service.getContractSpecEntries).not.toHaveBeenCalled();
+      expect(result).toEqual([]);
+    });
+    it('Should return the smart contract functions', async () => {
+      jest
+        .spyOn(service, 'getInstanceValue')
+        .mockResolvedValue(contractExecutable);
+      jest.spyOn(contractExecutable, 'switch').mockReturnValue({
+        name: CONTRACT_EXECUTABLE_TYPE.WASM,
+        value: 0,
+      });
+      jest.spyOn(service, 'getStellarAssetContractFunctions');
+      jest.spyOn(service, 'getContractSpecEntries').mockResolvedValue([]);
+      jest.spyOn(service, 'extractFunctionInfo').mockReturnValue({
+        name: '',
+        docs: '',
+        inputs: [],
+        outputs: [],
+      });
+
+      const result = await service.generateMethodsFromContractId('contractId');
+
+      expect(service.getInstanceValue).toHaveBeenCalled();
+      expect(service.getStellarAssetContractFunctions).not.toHaveBeenCalled();
+      expect(service.getContractSpecEntries).toHaveBeenCalled();
+      expect(result).toEqual([]);
     });
   });
 });
