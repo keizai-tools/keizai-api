4e80b83d42c7e79b79037d450422da42a88e725c
Fede Estigarribia <fede.estigarribia@hotmail.com>
2024-04-12 17:34:51 -0300

    Add stellarAdapter

---
+++ src/common/application/repository/stellar.adapter.interface.ts
@@ -0,0 +1,25 @@
+import { ContractSpec, Transaction, xdr } from 'stellar-sdk';
+
+import {
+  EncodeEvent,
+  RawGetTransactionResponse,
+  RawSendTransactionResponse,
+} from '../types/soroban';
+
+export interface IStellarAdapter {
+  changeNetwork(selectedNetwork: string): void;
+  getContractSpec(specEntries: xdr.ScSpecEntry[]): ContractSpec;
+  getScSpecEntryFromXDR(input: string): xdr.ScSpecEntry;
+  getInstanceValue(contractId: string): Promise<xdr.ContractExecutable>;
+  getWasmCode(instance: xdr.ContractExecutable): Promise<Buffer>;
+  getContractEvents(contractId: string): Promise<EncodeEvent[]>;
+  prepareTransaction(
+    publicKey: string,
+    contractId: string,
+    methodName: string,
+    scArgs: xdr.ScVal[],
+  ): Promise<Transaction>;
+  signTransaction(tx: Transaction, secretKey: string): void;
+  rawSendTransaction(tx: Transaction): Promise<RawSendTransactionResponse>;
+  rawGetTransaction(hash: string): Promise<RawGetTransactionResponse>;
+}
