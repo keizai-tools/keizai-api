843cb03530751ca66f2ceb1637416645519b453c
nahuelRosas <nahuel.rosas@mi.unc.edu.ar>
2024-07-23 18:07:34 -0300

    feat: Add CreateUserDto for user creation
    This commit adds the `CreateUserDto` class to the `create_user.dto.ts` file in the `src/modules/user/application/dto` directory. The DTO includes properties for email, password, isVerified, and externalId. It also includes validation decorators for email format, password complexity, and optional fields.
    
    These changes enable the application to have a standardized data transfer object for user creation, ensuring data integrity and validation.

---
+++ src/modules/user/application/dto/create_user.dto.ts
@@ -0,0 +1,32 @@
+import {
+  IsBoolean,
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Matches,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail({}, { message: 'invalid email address.' })
+  @IsNotEmpty()
+  email: string;
+
+  @Matches(
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$&+,:;=?@#|'<>.^*()%!-])[A-Za-z\d$&+,:;=@#|'<>.^*()%!-]{8,}$/,
+    {
+      message:
+        'password must contain at least 8 characters, including at least one lowercase letter, one uppercase letter, one number, and one special character.',
+    },
+  )
+  @IsNotEmpty()
+  password: string;
+
+  @IsOptional()
+  @IsBoolean()
+  isVerified: boolean;
+
+  @IsOptional()
+  @IsString()
+  externalId?: string;
+}
