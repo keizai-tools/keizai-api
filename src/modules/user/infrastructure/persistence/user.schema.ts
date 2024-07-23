4a0faa0421b30fd39e5792f137e8266b43d8cec3
nahuelRosas <nahuel.rosas@mi.unc.edu.ar>
2024-07-23 18:10:43 -0300

    feat: Add UserSchema for user data persistence

---
+++ src/modules/user/infrastructure/persistence/user.schema.ts
@@ -0,0 +1,47 @@
+import { EntitySchema } from 'typeorm';
+
+import { baseColumnSchemas } from '@/common/base/infrastructure/persistence/base.schema';
+
+import { User } from '../../domain/user.domain';
+
+export const UserSchema = new EntitySchema<User>({
+  name: 'User',
+  target: User,
+  columns: {
+    ...baseColumnSchemas,
+    email: {
+      type: 'varchar',
+      name: 'email',
+    },
+    externalId: {
+      type: 'varchar',
+      name: 'external_id',
+    },
+  },
+  relations: {
+    collections: {
+      target: 'Collection',
+      type: 'one-to-many',
+      joinColumn: {
+        name: 'user_id',
+      },
+    },
+    folders: {
+      target: 'Folder',
+      type: 'one-to-many',
+      joinColumn: {
+        name: 'user_id',
+      },
+    },
+    memberTeams: {
+      target: 'UserRoleToTeam',
+      type: 'one-to-many',
+      inverseSide: 'user',
+    },
+    invitationsReceived: {
+      target: 'Invitation',
+      type: 'one-to-many',
+      inverseSide: 'toUser',
+    },
+  },
+});
