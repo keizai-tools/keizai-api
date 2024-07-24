8d685c42fc00dd3e456eb892a8540214c300b3d3
nahuelRosas <nahuel.rosas@mi.unc.edu.ar>
2024-07-23 22:07:08 -0300

    feat: Add IAuthController interface for authentication controller
    This commit adds the `IAuthController` interface to the `auth.controller.interface.ts` file in the `src/modules/auth/application/interfaces` directory. The interface defines methods for user registration, login, user confirmation, password reset, session refresh, and more.
    
    These changes enable the application to have a standardized interface for the authentication controller, improving code organization and reusability.

---
+++ src/modules/auth/application/interfaces/auth.controller.interface.ts
@@ -0,0 +1,37 @@
+import { PasswordResetConfirmationDto } from '@/common/cognito/application/dto/password_reset_confirmation.dto';
+import { PasswordResetRequestDto } from '@/common/cognito/application/dto/password_reset_request.dto';
+import { ResendConfirmationDetailsDto } from '@/common/cognito/application/dto/resend_confirmation_details.dto';
+import { SessionRefreshDetailsDto } from '@/common/cognito/application/dto/session_refresh_details.dto';
+import { UserConfirmationDetailsDto } from '@/common/cognito/application/dto/user_confirmation_details.dto';
+import { UserLoginCredentialsDto } from '@/common/cognito/application/dto/user_login_credentials.dto';
+import { UserRegistrationDetailsDto } from '@/common/cognito/application/dto/user_registration_details.dto';
+import { IPromiseResponse } from '@/common/response_service/interface/response.interface';
+import { User } from '@/modules/user/domain/user.domain';
+
+export interface IAuthController {
+  registerUser(
+    userRegistrationDetails: UserRegistrationDetailsDto,
+  ): IPromiseResponse<User>;
+  loginUser(userLoginCredentials: UserLoginCredentialsDto): IPromiseResponse<{
+    accessToken: string;
+    refreshToken: string;
+    user: User;
+  }>;
+  confirmUserRegistration(
+    userConfirmationDetails: UserConfirmationDetailsDto,
+  ): IPromiseResponse<User>;
+  resendUserConfirmationCode(
+    resendConfirmationDetails: ResendConfirmationDetailsDto,
+  ): IPromiseResponse<void>;
+  initiatePasswordReset(
+    passwordResetRequest: PasswordResetRequestDto,
+  ): IPromiseResponse<void>;
+  confirmPasswordReset(
+    passwordResetConfirmation: PasswordResetConfirmationDto,
+  ): IPromiseResponse<void>;
+  refreshUserSession(
+    sessionRefreshDetails: SessionRefreshDetailsDto,
+  ): IPromiseResponse<{
+    accessToken: string;
+  }>;
+}
