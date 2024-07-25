875b93ce2c91b40414edd1ab37d17879f8d119b4
nahuelRosas <nahuel.rosas@mi.unc.edu.ar>
2024-07-25 17:18:11 -0300

    refactor: Rename folder repository file to improve clarity and consistency

--- src/modules/folder/application/interface/folder.repository.interface.ts
+++ src/modules/folder/application/interface/folder.repository.interface.ts
@@ -1,12 +1,12 @@
-import { IBaseRepository } from '@/common/application/base.repository';
-
 import { Folder } from '../../domain/folder.domain';
 
-export interface IFolderRepository extends IBaseRepository<Folder> {
+export const FOLDER_REPOSITORY = 'FOLDER_REPOSITORY';
+
+export interface IFolderRepository {
+  save(folder: Folder): Promise<Folder>;
+  findOne(id: string): Promise<Folder>;
   findOneByFolderAndUserId(id: string, userId: string): Promise<Folder>;
   findOneByFolderAndTeamId(id: string, teamId: string): Promise<Folder>;
   update(folder: Folder): Promise<Folder>;
   delete(id: string): Promise<boolean>;
 }
-
-export const FOLDER_REPOSITORY = 'FOLDER_REPOSITORY';
