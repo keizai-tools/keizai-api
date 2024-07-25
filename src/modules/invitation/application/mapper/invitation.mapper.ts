fd0bd964403825f43e4d0e7f87d18c221053e72e
nahuelRosas <nahuel.rosas@mi.unc.edu.ar>
2024-07-25 18:47:17 -0300

    refactor: Update invitation.mapper.ts to implement IInvitationMapper interface
    This commit updates the `invitation.mapper.ts` file to implement the `IInvitationMapper` interface. By doing so, it ensures that the `InvitationMapper` class adheres to the contract defined by the interface. This change improves the modularity and maintainability of the codebase.

--- src/modules/invitation/application/mapper/invitation.mapper.ts
+++ src/modules/invitation/application/mapper/invitation.mapper.ts
@@ -2,8 +2,9 @@ import { Invitation } from '../../domain/invitation.domain';
 import { CreateInvitationDto } from '../dto/create-invitation.dto';
 import { ResponseInvitationDto } from '../dto/response-invitation.dto';
 import { UpdateInvitationDto } from '../dto/update-invitation.dto';
+import { IInvitationMapper } from '../interface/invitation.mapper.interface';
 
-export class InvitationMapper {
+export class InvitationMapper implements IInvitationMapper {
   fromDtoToEntity(invitationDto: CreateInvitationDto): Invitation {
     const { teamId, fromUserId, toUserId, status } = invitationDto;
     return new Invitation(teamId, fromUserId, toUserId, status);
