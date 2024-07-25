d9f8047d97d7699dbbed7df50593c43a8c98d6c5
nahuelRosas <nahuel.rosas@mi.unc.edu.ar>
2024-07-25 16:26:45 -0300

    refactor: Update EnviromentModule imports
    This commit adds the CommonModule to the imports of the EnviromentModule in the enviroment.module.ts file. This change improves the functionality and maintainability of the code.

--- src/modules/enviroment/enviroment.module.ts
+++ src/modules/enviroment/enviroment.module.ts
@@ -8,8 +8,8 @@ import { TeamModule } from '../team/team.module';
 import { EnviromentMapper } from './application/mapper/enviroment.mapper';
 import { ENVIROMENT_REPOSITORY } from './application/repository/enviroment.repository';
 import { EnviromentService } from './application/service/enviroment.service';
+import { EnviromentRepository } from './infrastructure/persistence/enviroment.repository';
 import { EnviromentSchema } from './infrastructure/persistence/enviroment.schema';
-import { EnviromentRepository } from './infrastructure/persistence/enviroment.typeorm.repository';
 import { EnviromentUserController } from './interface/enviroment.controller';
 import { EnviromentTeamController } from './interface/environment-team.controller';
 
