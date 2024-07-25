f188c4b9cb2cab31d18a529fbba387aa187c4a6c
nahuelRosas <nahuel.rosas@mi.unc.edu.ar>
2024-07-25 17:13:59 -0300

    refactor: Update FolderTeamController
    Refactor FolderTeamController to use @Auth decorator and improve code structure

--- src/modules/folder/interface/folder-team.controller.ts
+++ src/modules/folder/interface/folder-team.controller.ts
@@ -6,53 +6,60 @@ import {
   Param,
   Patch,
   Post,
-  UseGuards,
 } from '@nestjs/common';
 
-import { AdminRoleGuard } from '@/modules/auth/infrastructure/guard/admin-role.guard';
-import { AuthTeamGuard } from '@/modules/auth/infrastructure/guard/auth-team.guard';
-import { JwtAuthGuard } from '@/modules/auth/infrastructure/guard/policy-auth.guard';
+import { IPromiseResponse } from '@/common/response_service/interface/response.interface';
+import { Auth } from '@/modules/auth/application/decorator/auth.decorator';
+import { AuthType } from '@/modules/auth/domain/auth_type.enum';
+import { Invocation } from '@/modules/invocation/domain/invocation.domain';
 
 import { CreateFolderDto } from '../application/dto/create-folder.dto';
+import { FolderResponseDto } from '../application/dto/folder-response.dto';
 import { UpdateFolderDto } from '../application/dto/update-folder.dto';
 import { FolderService } from '../application/service/folder.service';
 
+@Auth(AuthType.Bearer)
 @Controller('/team/:teamId/folder')
-@UseGuards(JwtAuthGuard, AuthTeamGuard)
 export class FolderTeamController {
   constructor(private readonly folderService: FolderService) {}
 
-  @UseGuards(AdminRoleGuard)
   @Post('')
   async create(
     @Body() createFolderDto: CreateFolderDto,
     @Param('teamId') teamId: string,
-  ) {
+  ): IPromiseResponse<FolderResponseDto> {
     return this.folderService.createByTeam(createFolderDto, teamId);
   }
 
   @Get('/:id')
-  findOne(@Param('teamId') teamId: string, @Param('id') id: string) {
+  findOne(
+    @Param('teamId') teamId: string,
+    @Param('id') id: string,
+  ): IPromiseResponse<FolderResponseDto> {
     return this.folderService.findOneByFolderAndTeamId(id, teamId);
   }
 
   @Get('/:id/invocations')
-  findAllInvocations(@Param('teamId') teamId: string, @Param('id') id: string) {
+  findAllInvocations(
+    @Param('teamId') teamId: string,
+    @Param('id') id: string,
+  ): IPromiseResponse<Invocation[]> {
     return this.folderService.findAllInvocationsByTeam(id, teamId);
   }
 
-  @UseGuards(AdminRoleGuard)
   @Patch('')
   update(
     @Body() updateFoldertDto: UpdateFolderDto,
     @Param('teamId') teamId: string,
-  ) {
+  ): IPromiseResponse<FolderResponseDto> {
     return this.folderService.updateByTeam(updateFoldertDto, teamId);
   }
 
-  @UseGuards(AdminRoleGuard)
   @Delete('/:id')
-  delete(@Param('teamId') teamId: string, @Param('id') id: string) {
+  delete(
+    @Param('teamId') teamId: string,
+    @Param('id') id: string,
+  ): IPromiseResponse<boolean> {
     return this.folderService.deleteByTeam(id, teamId);
   }
 }
