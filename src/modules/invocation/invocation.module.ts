322aedd21a51f358f20306dc0f04b4b8a382990e
nahuelRosas <nahuel.rosas@mi.unc.edu.ar>
2024-07-25 19:20:24 -0300

    refactor: Improve modularity and maintainability of invocation-related code

--- src/modules/invocation/invocation.module.ts
+++ src/modules/invocation/invocation.module.ts
@@ -8,11 +8,14 @@ import { FolderModule } from '../folder/folder.module';
 import { MethodModule } from '../method/method.module';
 import { TeamModule } from '../team/team.module';
 import { InvocationException } from './application/exceptions/invocation.exceptions';
+import { INVOCATION_EXCEPTION } from './application/interface/invocation.exceptions.interface';
+import { INVOCATION_MAPPER } from './application/interface/invocation.mapper.interface';
+import { INVOCATION_REPOSITORY } from './application/interface/invocation.repository.interface';
+import { INVOCATION_SERVICE } from './application/interface/invocation.service.interface';
 import { InvocationMapper } from './application/mapper/invocation.mapper';
-import { INVOCATION_REPOSITORY } from './application/repository/invocation.repository';
 import { InvocationService } from './application/service/invocation.service';
 import { InvocationSchema } from './infrastructure/persistence/invocation.schema';
-import { InvocationRepository } from './infrastructure/persistence/invocation.typeorm.repository';
+import { InvocationRepository } from './infrastructure/persistence/invocation.repository';
 import { InvocationTeamController } from './interface/invocation-team.controller';
 import { InvocationUserController } from './interface/invocation.controller';
 
@@ -27,17 +30,32 @@ import { InvocationUserController } from './interface/invocation.controller';
   ],
   controllers: [InvocationUserController, InvocationTeamController],
   providers: [
-    InvocationException,
-    InvocationService,
-    InvocationMapper,
+    {
+      provide: INVOCATION_EXCEPTION,
+      useClass: InvocationException,
+    },
+    {
+      provide: INVOCATION_SERVICE,
+      useClass: InvocationService,
+    },
+    {
+      provide: INVOCATION_MAPPER,
+      useClass: InvocationMapper,
+    },
     {
       provide: INVOCATION_REPOSITORY,
       useClass: InvocationRepository,
     },
   ],
   exports: [
-    InvocationService,
-    InvocationMapper,
+    {
+      provide: INVOCATION_SERVICE,
+      useClass: InvocationService,
+    },
+    {
+      provide: INVOCATION_MAPPER,
+      useClass: InvocationMapper,
+    },
     {
       provide: INVOCATION_REPOSITORY,
       useClass: InvocationRepository,
