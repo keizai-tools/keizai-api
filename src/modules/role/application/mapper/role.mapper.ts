19a07d6b101870537c7c0225b38b37c1c2e90c7e
nahuelRosas <nahuel.rosas@mi.unc.edu.ar>
2024-07-25 20:22:03 -0300

    refactor: Update role.mapper.ts to implement IUserRoleToTeamMapper interface

--- src/modules/role/application/mapper/role.mapper.ts
+++ src/modules/role/application/mapper/role.mapper.ts
@@ -1,11 +1,12 @@
 import { UserRoleToTeam } from '../../domain/role.domain';
 import { ResponseUserRoletoTeamDto } from '../dto/response-user-role.dto';
+import { IUserRoleToTeamMapper } from '../interface/role.mapper.interface';
 import {
   IUpdateUserRoleToTeamData,
   UserRoleToTeamData,
-} from '../service/role.service';
+} from '../interface/role.service.interface';
 
-export class UserRoleToTeamMapper {
+export class UserRoleToTeamMapper implements IUserRoleToTeamMapper {
   fromDtoToEntity(userRoleData: UserRoleToTeamData): UserRoleToTeam {
     const { teamId, userId, role } = userRoleData;
     return new UserRoleToTeam(teamId, userId, role);
@@ -17,6 +18,7 @@ export class UserRoleToTeamMapper {
     const { teamId, userId, role, id } = userRoleData;
     return new UserRoleToTeam(teamId, userId, role, id);
   }
+
   fromEntityToDto(userRole: UserRoleToTeam): ResponseUserRoletoTeamDto {
     const { id, teamId, userId, role } = userRole;
     return new ResponseUserRoletoTeamDto(id, teamId, userId, role);
