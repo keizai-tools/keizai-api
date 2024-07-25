c5a0be921c3cc5e87a14d4a788540aca0d0aef42
nahuelRosas <nahuel.rosas@mi.unc.edu.ar>
2024-07-25 18:47:59 -0300

    refactor: Update invitation module imports and providers

--- src/modules/invitation/application/service/invitation.service.ts
+++ src/modules/invitation/application/service/invitation.service.ts
@@ -5,121 +5,186 @@ import {
   NotFoundException,
 } from '@nestjs/common';
 
+import {
+  IPromiseResponse,
+  IResponseService,
+  RESPONSE_SERVICE,
+} from '@/common/response_service/interface/response.interface';
+
 import { Invitation } from '../../domain/invitation.domain';
 import { CreateInvitationDto } from '../dto/create-invitation.dto';
 import { ResponseInvitationDto } from '../dto/response-invitation.dto';
 import { UpdateInvitationDto } from '../dto/update-invitation.dto';
 import { INVITATION_RESPONSE } from '../exceptions/invitation.enum';
-import { InvitationMapper } from '../mapper/invitation.mapper';
+import {
+  IInvitationMapper,
+  INVITATION_MAPPER,
+} from '../interface/invitation.mapper.interface';
 import {
   IInvitationRepository,
   INVITATION_REPOSITORY,
-} from '../repository/invitation.repository';
+} from '../interface/invitation.repository.interface';
+import { IInvitationService } from '../interface/invitation.service.interface';
 
 @Injectable()
-export class InvitationService {
+export class InvitationService implements IInvitationService {
   constructor(
-    private readonly invitationMapper: InvitationMapper,
+    @Inject(INVITATION_MAPPER)
+    private readonly invitationMapper: IInvitationMapper,
+    @Inject(RESPONSE_SERVICE)
+    private readonly responseService: IResponseService,
     @Inject(INVITATION_REPOSITORY)
     private readonly invitationRepository: IInvitationRepository,
-  ) {}
-
-  async findAllByUserId(userId: string): Promise<ResponseInvitationDto[]> {
-    const invitations = await this.invitationRepository.findAllByUserId(userId);
-
-    if (!invitations) {
-      throw new NotFoundException(INVITATION_RESPONSE.INVITATIONS_NOT_FOUND);
-    }
-
-    return invitations.map((invitation) =>
-      this.invitationMapper.fromEntityToDto(invitation),
-    );
+  ) {
+    this.responseService.setContext(InvitationService.name);
   }
 
-  async findOne(id: string): Promise<ResponseInvitationDto> {
-    const invitation = await this.invitationRepository.findOne(id);
+  async findAllByUserId(
+    userId: string,
+  ): IPromiseResponse<ResponseInvitationDto[]> {
+    try {
+      const invitations = await this.invitationRepository.findAllByUserId(
+        userId,
+      );
 
-    if (!invitation) {
-      throw new NotFoundException(INVITATION_RESPONSE.INVITATION_NOT_FOUND);
+      if (!invitations) {
+        throw new NotFoundException(INVITATION_RESPONSE.INVITATIONS_NOT_FOUND);
+      }
+
+      return this.responseService.createResponse({
+        payload: invitations.map((invitation) =>
+          this.invitationMapper.fromEntityToDto(invitation),
+        ),
+        message: INVITATION_RESPONSE.INVITATIONS_FOUND,
+        type: 'OK',
+      });
+    } catch (error) {
+      this.handleError(error);
     }
+  }
 
-    return this.invitationMapper.fromEntityToDto(invitation);
+  async findOne(id: string): IPromiseResponse<ResponseInvitationDto> {
+    try {
+      const invitation = await this.invitationRepository.findOne(id);
+
+      if (!invitation) {
+        throw new NotFoundException(INVITATION_RESPONSE.INVITATION_NOT_FOUND);
+      }
+
+      return this.responseService.createResponse({
+        payload: this.invitationMapper.fromEntityToDto(invitation),
+        message: INVITATION_RESPONSE.INVITATION_FOUND,
+        type: 'OK',
+      });
+    } catch (error) {
+      this.handleError(error);
+    }
   }
 
   async create(
     invitationDto: CreateInvitationDto,
-  ): Promise<ResponseInvitationDto> {
-    const invitationMapped =
-      this.invitationMapper.fromDtoToEntity(invitationDto);
+  ): IPromiseResponse<ResponseInvitationDto> {
     try {
+      const invitationMapped =
+        this.invitationMapper.fromDtoToEntity(invitationDto);
+
       const invitationSaved = await this.invitationRepository.save(
         invitationMapped,
       );
-      return this.invitationMapper.fromEntityToDto(invitationSaved);
+
+      return this.responseService.createResponse({
+        payload: this.invitationMapper.fromEntityToDto(invitationSaved),
+        message: INVITATION_RESPONSE.INVITATION_CREATED,
+        type: 'CREATED',
+      });
     } catch (error) {
-      console.log(error);
-      throw new BadRequestException(INVITATION_RESPONSE.INVITATION_FAILED_SAVE);
+      this.handleError(error);
     }
   }
 
   async createAll(invitationsDto: CreateInvitationDto[]) {
-    const invitationsToSave: Invitation[] = invitationsDto.map((invitation) => {
-      return this.invitationMapper.fromDtoToEntity(invitation);
-    });
+    try {
+      const invitationsToSave: Invitation[] = invitationsDto.map(
+        (invitation) => {
+          return this.invitationMapper.fromDtoToEntity(invitation);
+        },
+      );
 
-    const invitationsSaved = await this.invitationRepository.saveAll(
-      invitationsToSave,
-    );
+      const invitationsSaved = await this.invitationRepository.saveAll(
+        invitationsToSave,
+      );
 
-    if (!invitationsSaved) {
-      throw new BadRequestException(INVITATION_RESPONSE.INVITATION_FAILED_SAVE);
+      if (!invitationsSaved) {
+        throw new BadRequestException(
+          INVITATION_RESPONSE.INVITATION_FAILED_SAVE,
+        );
+      }
+
+      return invitationsSaved.map((invitation) =>
+        this.invitationMapper.fromEntityToDto(invitation),
+      );
+    } catch (error) {
+      this.handleError(error);
     }
-
-    return invitationsSaved.map((invitation) =>
-      this.invitationMapper.fromEntityToDto(invitation),
-    );
   }
 
   async update(
     invitationDto: UpdateInvitationDto,
-  ): Promise<ResponseInvitationDto> {
-    const invitationMapped =
-      this.invitationMapper.fromUpdateDtoToEntity(invitationDto);
-    const invitationUpdated = await this.invitationRepository.update(
-      invitationMapped,
-    );
+  ): IPromiseResponse<ResponseInvitationDto> {
+    try {
+      const invitationMapped =
+        this.invitationMapper.fromUpdateDtoToEntity(invitationDto);
 
-    if (!invitationUpdated) {
-      throw new BadRequestException(
-        INVITATION_RESPONSE.INVITATION_FAILED_UPDATED,
+      const invitationUpdated = await this.invitationRepository.update(
+        invitationMapped,
       );
+
+      if (!invitationUpdated) {
+        throw new BadRequestException(
+          INVITATION_RESPONSE.INVITATION_FAILED_UPDATED,
+        );
+      }
+
+      return this.responseService.createResponse({
+        payload: this.invitationMapper.fromEntityToDto(invitationUpdated),
+        message: INVITATION_RESPONSE.INVITATION_UPDATED,
+        type: 'ACCEPTED',
+      });
+    } catch (error) {
+      this.handleError(error);
     }
-
-    const invitationSaved = await this.invitationRepository.save(
-      invitationUpdated,
-    );
-
-    if (!invitationSaved) {
-      throw new BadRequestException(INVITATION_RESPONSE.INVITATION_FAILED_SAVE);
-    }
-
-    return this.invitationMapper.fromEntityToDto(invitationSaved);
   }
 
-  async delete(id: string): Promise<boolean> {
-    const invitation = await this.invitationRepository.findOne(id);
+  async delete(id: string): IPromiseResponse<boolean> {
+    try {
+      const invitation = await this.invitationRepository.findOne(id);
 
-    if (!invitation) {
-      throw new NotFoundException(INVITATION_RESPONSE.INVITATION_NOT_FOUND);
+      if (!invitation) {
+        throw new NotFoundException(INVITATION_RESPONSE.INVITATION_NOT_FOUND);
+      }
+
+      const invitationDeleted = await this.invitationRepository.delete(id);
+
+      if (!invitationDeleted) {
+        throw new BadRequestException(
+          INVITATION_RESPONSE.INVITATION_FAILED_DELETED,
+        );
+      }
+
+      return this.responseService.createResponse({
+        payload: invitationDeleted,
+        message: INVITATION_RESPONSE.INVITATION_DELETED,
+        type: 'ACCEPTED',
+      });
+    } catch (error) {
+      this.handleError(error);
     }
+  }
 
-    const invitationDeleted = await this.invitationRepository.delete(id);
-
-    if (!invitationDeleted) {
-      throw new BadRequestException(
-        INVITATION_RESPONSE.INVITATION_FAILED_DELETED,
-      );
-    }
-    return invitationDeleted;
+  private handleError(error: Error): void {
+    this.responseService.errorHandler({
+      type: 'INTERNAL_SERVER_ERROR',
+      error,
+    });
   }
 }
