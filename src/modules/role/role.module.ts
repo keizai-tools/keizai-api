4ffe073e7908ee5bb2cf27c43fb22f093b694e71
nahuelRosas <nahuel.rosas@mi.unc.edu.ar>
2024-07-25 20:25:08 -0300

    refactor: Update role.module.ts to improve modularity and maintainability

--- src/modules/role/role.module.ts
+++ src/modules/role/role.module.ts
@@ -1,24 +1,47 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { CommonModule } from '@/common/common.module';
+
+import { USER_ROLE_TO_TEAM_MAPPER } from './application/interface/role.mapper.interface';
+import { USER_ROLE_TO_TEAM_REPOSITORY } from './application/interface/role.repository.interface';
+import { USER_ROLE_TO_TEAM_SERVICE } from './application/interface/role.service.interface';
 import { UserRoleToTeamMapper } from './application/mapper/role.mapper';
-import { USER_ROLE_TO_TEAM_REPOSITORY } from './application/repository/role.repository';
 import { UserRoleOnTeamService } from './application/service/role.service';
+import { UserRoleToTeamRepository } from './infrastructure/persistence/role.repository';
 import { UserRoleToTeamSchema } from './infrastructure/persistence/role.schema';
-import { UserRoleToTeamRepository } from './infrastructure/persistence/role.typeorm.repository';
 import { UserRoleToTeamController } from './interface/role.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRoleToTeamSchema])],
+  imports: [
+    TypeOrmModule.forFeature([UserRoleToTeamSchema]),
+    forwardRef(() => CommonModule),
+  ],
   controllers: [UserRoleToTeamController],
   providers: [
-    UserRoleOnTeamService,
-    UserRoleToTeamMapper,
+    {
+      provide: USER_ROLE_TO_TEAM_SERVICE,
+      useClass: UserRoleOnTeamService,
+    },
+    {
+      provide: USER_ROLE_TO_TEAM_MAPPER,
+      useClass: UserRoleToTeamMapper,
+    },
+    ,
     {
       provide: USER_ROLE_TO_TEAM_REPOSITORY,
       useClass: UserRoleToTeamRepository,
     },
   ],
-  exports: [UserRoleOnTeamService, UserRoleToTeamMapper],
+  exports: [
+    {
+      provide: USER_ROLE_TO_TEAM_SERVICE,
+      useClass: UserRoleOnTeamService,
+    },
+    {
+      provide: USER_ROLE_TO_TEAM_MAPPER,
+      useClass: UserRoleToTeamMapper,
+    },
+  ],
 })
 export class UserRoleToTeamModule {}
