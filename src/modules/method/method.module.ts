4063b023b43ab498e055588e9b07877ee4aadc8e
nahuelRosas <nahuel.rosas@mi.unc.edu.ar>
2024-07-25 19:51:35 -0300

    refactor: Update method.module.ts to improve modularity and maintainability
    This commit updates the `method.module.ts` file to improve the modularity and maintainability of the codebase. Changes include:
    - Importing the `CommonModule` from `'@/common/common.module'`
    - Updating the providers and exports to use the `METHOD_SERVICE`, `METHOD_MAPPER`, and `METHOD_REPOSITORY` tokens
    - Using the `MethodRepository` class from `'./infrastructure/persistence/method.repository'` instead of the deprecated `MethodTypeOrmRepository`
    
    These changes enhance the organization and readability of the code, making it easier to understand and maintain.

--- src/modules/method/method.module.ts
+++ src/modules/method/method.module.ts
@@ -1,13 +1,17 @@
 import { Module, forwardRef } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { CommonModule } from '@/common/common.module';
+
 import { InvocationModule } from '../invocation/invocation.module';
 import { TeamModule } from '../team/team.module';
+import { METHOD_MAPPER } from './application/interface/method.mapper.interface';
+import { METHOD_REPOSITORY } from './application/interface/method.repository.interface';
+import { METHOD_SERVICE } from './application/interface/method.service.interface';
 import { MethodMapper } from './application/mapper/method.mapper';
-import { METHOD_REPOSITORY } from './application/repository/method.interface.repository';
 import { MethodService } from './application/service/method.service';
+import { MethodRepository } from './infrastructure/persistence/method.repository';
 import { MethodSchema } from './infrastructure/persistence/method.schema';
-import { MethodRepository } from './infrastructure/persistence/method.typeorm.repository';
 import { MethodTeamController } from './interface/method-team.controller';
 import { MethodUserController } from './interface/method.controller';
 
@@ -15,17 +19,38 @@ import { MethodUserController } from './interface/method.controller';
   imports: [
     TypeOrmModule.forFeature([MethodSchema]),
     forwardRef(() => InvocationModule),
+    forwardRef(() => CommonModule),
+
     TeamModule,
   ],
   controllers: [MethodUserController, MethodTeamController],
   providers: [
-    MethodService,
-    MethodMapper,
+    {
+      provide: METHOD_SERVICE,
+      useClass: MethodService,
+    },
+    {
+      provide: METHOD_MAPPER,
+      useClass: MethodMapper,
+    },
     {
       provide: METHOD_REPOSITORY,
       useClass: MethodRepository,
     },
   ],
-  exports: [MethodMapper, METHOD_REPOSITORY, MethodService],
+  exports: [
+    {
+      provide: METHOD_SERVICE,
+      useClass: MethodService,
+    },
+    {
+      provide: METHOD_MAPPER,
+      useClass: MethodMapper,
+    },
+    {
+      provide: METHOD_REPOSITORY,
+      useClass: MethodRepository,
+    },
+  ],
 })
 export class MethodModule {}
