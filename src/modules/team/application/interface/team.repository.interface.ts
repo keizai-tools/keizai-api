956e301690f71eb9e4bca7e498a955505e5023ad
nahuelRosas <nahuel.rosas@mi.unc.edu.ar>
2024-07-26 13:18:09 -0300

    refactor: Rename team.repository.ts to team.interface.ts
    The file `team.repository.ts` has been renamed to `team.interface.ts` to improve the clarity and consistency of the codebase. This change ensures that the file name accurately reflects its purpose as an interface for the team repository.

--- src/modules/team/application/interface/team.repository.interface.ts
+++ src/modules/team/application/interface/team.repository.interface.ts
@@ -1,13 +1,12 @@
-import { IBaseRepository } from '@/common/application/base.repository';
-
 import { Team } from '../../domain/team.domain';
 
-export interface ITeamRepository extends IBaseRepository<Team> {
+export const TEAM_REPOSITORY = 'TEAM_REPOSITORY';
+
+export interface ITeamRepository {
   findAllByUser(userId: string): Promise<Team[]>;
   findOne(id: string): Promise<Team>;
   findOneByIds(id: string, adminId: string): Promise<Team>;
   update(team: Team): Promise<Team>;
   delete(id: string): Promise<boolean>;
+  save(team: Team): Promise<Team>;
 }
-
-export const TEAM_REPOSITORY = 'TEAM_REPOSITORY';
