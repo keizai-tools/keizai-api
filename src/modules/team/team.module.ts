b2e5ef2b20a11b555b34bce1ceb2a9255bb7f6a3
nahuelRosas <nahuel.rosas@mi.unc.edu.ar>
2024-07-26 13:19:11 -0300

    refactor: Update team.module.ts to implement team service and mapper interfaces
    Update the `team.module.ts` file to implement the `ITeamService` and `ITeamMapper` interfaces. This change ensures that the module uses the correct service and mapper implementations, improving the modularity and maintainability of the codebase.

--- src/modules/team/team.module.ts
+++ src/modules/team/team.module.ts
@@ -1,15 +1,20 @@
 import { Module, forwardRef } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { CommonModule } from '@/common/common.module';
+
 import { AuthModule } from '../auth/auth.module';
 import { CollectionModule } from '../collection/collection.module';
 import { InvitationModule } from '../invitation/invitation.module';
 import { UserRoleToTeamModule } from '../role/role.module';
+import { UserModule } from '../user/user.module';
+import { TEAM_MAPPER } from './application/interface/team.mapper.interface';
+import { TEAM_REPOSITORY } from './application/interface/team.repository.interface';
+import { TEAM_SERVICE } from './application/interface/team.service.interface';
 import { TeamMapper } from './application/mapper/team.mapper';
-import { TEAM_REPOSITORY } from './application/repository/team.repository';
 import { TeamService } from './application/service/team.service';
+import { TeamRepository } from './infrastructure/persistence/team.repository';
 import { TeamSchema } from './infrastructure/persistence/team.schema';
-import { TeamRepository } from './infrastructure/persistence/team.typeorm.repository';
 import { TeamController } from './interface/team.controller';
 
 @Module({
@@ -19,16 +24,29 @@ import { TeamController } from './interface/team.controller';
     forwardRef(() => AuthModule),
     forwardRef(() => InvitationModule),
     forwardRef(() => UserRoleToTeamModule),
+    forwardRef(() => UserModule),
+    forwardRef(() => CommonModule),
   ],
   controllers: [TeamController],
   providers: [
-    TeamService,
-    TeamMapper,
+    {
+      provide: TEAM_MAPPER,
+      useClass: TeamMapper,
+    },
+    {
+      provide: TEAM_SERVICE,
+      useClass: TeamService,
+    },
     {
       provide: TEAM_REPOSITORY,
       useClass: TeamRepository,
     },
   ],
-  exports: [TeamService],
+  exports: [
+    {
+      provide: TEAM_SERVICE,
+      useClass: TeamService,
+    },
+  ],
 })
 export class TeamModule {}
