f7282ff9f80d60ffb2ec6274ea130893347b893d
nahuelRosas <nahuel.rosas@mi.unc.edu.ar>
2024-07-27 21:43:36 -0300

    refactor: Update collection.module.ts to use forwardRef() for importing TeamModule
    The file `collection.module.ts` has been updated to use `forwardRef()` for importing `TeamModule`. This change ensures that the module is imported correctly, improving the modularity and maintainability of the codebase.

--- src/modules/collection/collection.module.ts
+++ src/modules/collection/collection.module.ts
@@ -22,7 +22,7 @@ import { CollectionController } from './interface/collection.controller';
     forwardRef(() => FolderModule),
     forwardRef(() => EnviromentModule),
     forwardRef(() => CommonModule),
-    TeamModule,
+    forwardRef(() => TeamModule),
   ],
   controllers: [CollectionController, CollectionTeamController],
   providers: [
