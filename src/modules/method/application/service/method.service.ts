3f75365e7c50494829deb10bca8376a7ed6d32f2
nahuelRosas <nahuel.rosas@mi.unc.edu.ar>
2024-07-29 15:58:16 -0300

    refactor: Update imports in method.service.ts
    The code changes update the imports in method.service.ts to use the correct path for the IUpdateInvocationValues interface and MethodMapper class. This ensures that the correct dependencies are being used in the codebase and avoids potential compilation errors.

--- src/modules/method/application/service/method.service.ts
+++ src/modules/method/application/service/method.service.ts
@@ -11,7 +11,7 @@ import {
   IResponseService,
   RESPONSE_SERVICE,
 } from '@/common/response_service/interface/response.interface';
-import type { IUpdateInvocationValues } from '@/modules/invocation/application/interface/invocation.base.interface';
+import { IUpdateInvocationValues } from '@/modules/invocation/application/interface/invocation.base.interface';
 import {
   IInvocationRepository,
   INVOCATION_REPOSITORY,
@@ -33,7 +33,7 @@ import {
   IMethodRepository,
   METHOD_REPOSITORY,
 } from '../interface/method.repository.interface';
-import type { MethodMapper } from '../mapper/method.mapper';
+import { MethodMapper } from '../mapper/method.mapper';
 
 @Injectable()
 export class MethodService {
