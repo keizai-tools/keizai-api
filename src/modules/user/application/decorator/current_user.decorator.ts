d887c8c4331b27d43c9b1271b6fe049469b04821
Nahuel Rosas <nahuel.rosas@mi.unc.edu.ar>
2024-07-29 12:03:40 -0300

    Feat / User Module (#111)
    ## Summary
    
    This update introduces various features for user management in the
    NestJS application. It includes the implementation of DTOs for user
    creation and updating, interfaces for controllers, services, and
    repositories, as well as a new decorator for accessing the current user.
    Additionally, it updates the user domain and persistence layers, and
    adds service messages and enums for better management and error
    handling.
    
    ## Details
    
    ### Modifications
    
    1. **Added `CurrentUser` Decorator**:
       - Allows easy access to the current user in controllers.
    
    2. **Introduced `CreateUserDto`**:
    - Handles user creation with email, password, verification status, and
    external ID.
    
    3. **Added `UpdateUserDto`**:
       - Extends `CreateUserDto` for user updates with optional fields.
    
    4. **Implemented `IUpdateUserResponse` Interface**:
       - Defines the response structure for user updates.
    
    5. **Added `IUserController` Interface**:
       - Specifies the methods for user management in the controller.
    
    6. **Introduced `IUserMapper` Interface**:
       - Defines the method for mapping user data from DTOs.
    
    7. **Added `IUserRepository` Interface**:
       - Specifies methods for user data persistence and retrieval.
    
    8. **Implemented `IUserService` Interface**:
       - Defines methods for user creation, retrieval, and updates.
    
    9. **Created `UserMapper` Class**:
       - Maps user DTOs to user domain entities.
    
    10. **Added `ServiceMessage` Enum**:
        - Contains various messages related to user services.
    
    11. **Introduced `AuthType` Enum**:
        - Defines types of authentication.
    
    12. **Updated `User` Domain Class**:
    - Represents the user domain with properties for email, external ID, and
    more.
    
    13. **Implemented `UserRepository`**:
        - Handles user data persistence using TypeORM.
    
    14. **Created `UserSchema`**:
        - Defines the user schema for TypeORM.
    
    15. **Developed `UserController`**:
        - Manages user-related HTTP requests.
    
    16. **Added `UserService`**:
    - Provides user management services and integrates with the repository
    and mapper.
    
    17. **Configured `UserModule`**:
        - Sets up dependency injection for the user module.
    
    ### Changes Overview
    
    - **Files Added**: 17
    - **Lines Added**: 514
    - **Lines Deleted**: 0
    
    **File Changes:**
    - `src/modules/user/application/decorator/current_user.decorator.ts`: 12
    lines added
    - `src/modules/user/application/dto/create_user.dto.ts`: 32 lines added
    - `src/modules/user/application/dto/update_user.dto.ts`: 5 lines added
    - `src/modules/user/application/interfaces/user.common.interfaces.ts`: 6
    lines added
    -
    `src/modules/user/application/interfaces/user.controller.interface.ts`:
    10 lines added
    - `src/modules/user/application/interfaces/user.mapper.interfaces.ts`: 9
    lines added
    -
    `src/modules/user/application/interfaces/user.repository.interfaces.ts`:
    21 lines added
    - `src/modules/user/application/interfaces/user.service.interfaces.ts`:
    20 lines added
    - `src/modules/user/application/mapper/user.mapper.ts`: 14 lines added
    - `src/modules/user/application/message/user.message.ts`: 7 lines added
    - `src/modules/user/application/service/user.service.ts`: 124 lines
    added
    - `src/modules/user/domain/auth_type.enum.ts`: 6 lines added
    - `src/modules/user/domain/user.domain.ts`: 26 lines added
    - `src/modules/user/infrastructure/persistence/user.repository.ts`: 92
    lines added
    - `src/modules/user/infrastructure/persistence/user.schema.ts`: 47 lines
    added
    - `src/modules/user/interface/user.controller.ts`: 35 lines added
    - `src/modules/user/user.module.ts`: 48 lines added

---
+++ src/modules/user/application/decorator/current_user.decorator.ts
@@ -0,0 +1,12 @@
+import { ExecutionContext, createParamDecorator } from '@nestjs/common';
+
+import { REQUEST_USER_KEY } from '../../domain/auth_type.enum';
+import { User } from '../../domain/user.domain';
+
+export const CurrentUser = createParamDecorator(
+  (field: keyof User, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    const user: User | undefined = request[REQUEST_USER_KEY];
+    return field ? user?.[field] : user;
+  },
+);
---
+++ src/modules/user/domain/user.domain.ts
@@ -0,0 +1,29 @@
+import { Base } from '@/common/base/domain/base.domain';
+import { ENVIRONMENT } from '@/common/base/enum/common.enum';
+import { AppRole } from '@/modules/authorization/domain/role.enum';
+import { Collection } from '@/modules/collection/domain/collection.domain';
+import { Folder } from '@/modules/folder/domain/folder.domain';
+import { Invitation } from '@/modules/invitation/domain/invitation.domain';
+import { UserRoleToTeam } from '@/modules/role/domain/role.domain';
+
+export class User extends Base {
+  email: string;
+  externalId: string;
+  collections?: Collection[];
+  folders?: Folder[];
+  memberTeams?: UserRoleToTeam[];
+  invitationsReceived?: Invitation[];
+  isVerified: boolean;
+  roles: AppRole[];
+
+  constructor(email: string, externalId: string) {
+    super();
+    this.email = email;
+    this.externalId = externalId;
+    this.isVerified = !!(
+      process.env.NODE_ENV === ENVIRONMENT.DEVELOPMENT &&
+      process.env.COGNITO_POOL_TYPE === ENVIRONMENT.DEVELOPMENT
+    );
+    this.roles = [AppRole.Regular];
+  }
+}
