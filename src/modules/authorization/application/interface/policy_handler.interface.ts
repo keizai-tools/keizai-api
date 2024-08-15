ffaa8099d8e0993da539ef03455a7c7edafcbd2c
Nahuel Rosas <nahuel.rosas@mi.unc.edu.ar>
2024-08-15 10:22:02 -0300

    Refactor authorization module structure and simplify policy checks

--- src/modules/authorization/application/interface/policy_handler.interface.ts
+++
@@ -1,5 +0,0 @@
-import { Request } from 'express';
-
-export interface IPolicyHandler {
-  handle(request: Request): void;
-}
--- src/modules/authorization/infraestructure/casl/type/app-subjects.type.ts
+++
@@ -1,8 +0,0 @@
-import { InferSubjects } from '@casl/ability';
-import { Type } from '@nestjs/common';
-
-import { User } from '@/modules/user/domain/user.domain';
-
-export type AppSubjects = InferSubjects<typeof User> | 'all';
-
-export type Subjects = User | Type<User>;
