526601ed2d29dea5e8cee0d2ae9f9b6e471ec6b0
Nahuel Rosas <nahuel.rosas@mi.unc.edu.ar>
2024-08-15 10:28:43 -0300

    Refactor import paths and annotate API endpoints; Enhanced Swagger Docs

--- src/modules/method/interface/method-team.controller.ts
+++ src/modules/method/interface/method-team.controller.ts
@@ -8,11 +8,12 @@ import {
   Post,
   UseGuards,
 } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 
 import { IPromiseResponse } from '@/common/response_service/interface/response.interface';
 import { Auth } from '@/modules/auth/application/decorator/auth.decorator';
 import { AuthType } from '@/modules/auth/domain/auth_type.enum';
-import { AuthTeamGuard } from '@/modules/authorization/infraestructure/policy/guard/auth-team.guard';
+import { AuthTeamGuard } from '@/modules/authorization/guard/auth-team.guard';
 
 import { CreateMethodDto } from '../application/dto/create-method.dto';
 import { MethodResponseDto } from '../application/dto/method-response.dto';
@@ -20,6 +21,7 @@ import { UpdateMethodDto } from '../application/dto/update-method.dto';
 import { MethodService } from '../application/service/method.service';
 
 @Auth(AuthType.Bearer)
+@ApiTags('Method Team')
 @UseGuards(AuthTeamGuard)
 @Controller('/team/:teamId/method')
 export class MethodTeamController {
