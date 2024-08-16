ec4b3311eea877a0105179757c91f6930ca89b74
Nahuel Rosas <nahuel.rosas@mi.unc.edu.ar>
2024-08-16 14:14:29 -0300

    Refine Password Change DTO Validation and Naming

--- src/common/cognito/application/dto/change_password.dto.ts
+++ src/common/cognito/application/dto/change_password.dto.ts
@@ -1,21 +1,21 @@
-import { IsEmail, IsNotEmpty, Matches } from 'class-validator';
+import { IsJWT, IsNotEmpty, Matches } from 'class-validator';
 
 export class ChangePasswordDto {
-  @IsEmail({}, { message: 'invalid email address.' })
+  @IsJWT()
   @IsNotEmpty()
-  email: string;
+  accessToken: string;
 
   @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*\W)/, {
     message:
       'password must contain at least 8 characters, including at least one lowercase letter, one uppercase letter, one number, and one special character.',
   })
   @IsNotEmpty()
-  newPassword: string;
+  proposedPassword: string;
 
   @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*\W)/, {
     message:
       'password must contain at least 8 characters, including at least one lowercase letter, one uppercase letter, one number, and one special character.',
   })
   @IsNotEmpty()
-  oldPassword: string;
+  previousPassword: string;
 }
