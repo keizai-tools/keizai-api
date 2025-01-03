c81a547ff44561edfb12a25f8953341f9e0a9e7c
nahuelRosas <nahuel.rosas@mi.unc.edu.ar>
2025-01-03 11:41:54 -0300

    Adds Fargate access guard for user balance validation
    Introduces a guard to check if a user has sufficient balance to access Fargate resources based on the requested interval and cost per minute.
    
    Ensures users cannot proceed without meeting the balance requirement.

---
+++ src/modules/ephemeralEnvironment/application/guard/fargateAccessGuard.guard.ts
@@ -0,0 +1,44 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Inject,
+  Injectable,
+} from '@nestjs/common';
+
+import { IResponse } from '@/common/response_service/interface/response.interface';
+import { REQUEST_USER_KEY } from '@/modules/auth/domain/auth_type.enum';
+import {
+  IUserService,
+  USER_SERVICE,
+} from '@/modules/user/application/interfaces/user.service.interfaces';
+import { User } from '@/modules/user/domain/user.domain';
+
+@Injectable()
+export class FargateAccessGuard implements CanActivate {
+  constructor(
+    @Inject(USER_SERVICE)
+    private readonly userService: IUserService,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const user = request[REQUEST_USER_KEY] as IResponse<User>;
+    const interval = request.query.interval;
+
+    if (!user) {
+      return false;
+    }
+
+    const userBalanceResponse = await this.userService.getUserByEmail(
+      user.payload.email,
+    );
+
+    const userBalance = userBalanceResponse.payload.balance;
+
+    const costPerMinuteResponse = this.userService.getFargateCostPerMinute();
+
+    const requiredBalance = interval * costPerMinuteResponse;
+
+    return userBalance >= requiredBalance;
+  }
+}
