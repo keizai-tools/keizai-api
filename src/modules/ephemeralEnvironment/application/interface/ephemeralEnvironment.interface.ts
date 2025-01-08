55864d0846e07408808ed625589f980fc56bff69
nahuelRosas <nahuel.rosas@mi.unc.edu.ar>
2025-01-08 10:09:26 -0300

    Updates ephemeral environment service interface
    Adds new fields to ITaskInfo interface for task status and timestamps
    Modifies startTask and stopTask methods to include additional parameters
    Introduces new methods for retrieving task status and account information

--- src/modules/ephemeralEnvironment/application/interface/ephemeralEnvironment.interface.ts
+++ src/modules/ephemeralEnvironment/application/interface/ephemeralEnvironment.interface.ts
@@ -1,23 +1,28 @@
+import { type DesiredStatus, Task } from '@aws-sdk/client-ecs';
+
 import { IPromiseResponse } from '@/common/response_service/interface/response.interface';
 
 export interface ITaskInfo {
+  status: DesiredStatus;
   taskArn: string;
   publicIp: string;
+  taskStartedAt: Date;
+  taskStoppedAt: Date;
+  executionInterval: number;
 }
+
 export interface IEphemeralEnvironmentService {
-  startTask(clientId: string): Promise<IPromiseResponse<ITaskInfo>>;
+  startTask(
+    clientId: string,
+    intervalMinutes: number,
+  ): IPromiseResponse<ITaskInfo>;
   stopTask(
-    taskArn: string,
     clientId: string,
-  ): IPromiseResponse<{ taskArn: string }>;
-  getClientTask(clientId: string): Promise<ITaskInfo | null>;
-  deleteClientTask(clientId: string): Promise<void>;
-  saveClientTask(
-    clientId: string,
-    taskArn: string,
-    publicIp: string,
-  ): Promise<void>;
+  ): IPromiseResponse<{ taskArn: string; status: string }>;
   getTaskPublicIp(taskArn: string): IPromiseResponse<{ publicIp: string }>;
+  getClientTask(clientId: string): Promise<Task | null>;
+  getTaskStatus(clientId: string): IPromiseResponse<ITaskInfo>;
+  getAccountOrFund(publicKey: string, clientId: string): IPromiseResponse<void>;
 }
 
 export const EPHEMERAL_ENVIRONMENT_SERVICE = 'EPHEMERAL_ENVIRONMENT_SERVICE';
