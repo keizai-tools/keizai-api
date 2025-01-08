42a488f53ad51fc205f8373f75fe23a0c16b1522
nahuelRosas <nahuel.rosas@mi.unc.edu.ar>
2025-01-08 10:05:16 -0300

    Enhances S3 file upload service with user-specific metadata
    Adds user ID metadata to uploaded files
    Ensures bucket creation in development environment
    Improves file existence check with user validation
    Adds file metadata retrieval and download with user permissions
    Updates content type and encoding for file uploads

--- src/common/S3/service/file_upload.s3.service.ts
+++ src/common/S3/service/file_upload.s3.service.ts
@@ -1,5 +1,6 @@
 import { Inject } from '@nestjs/common';
-import { S3 } from 'aws-sdk';
+import { type AWSError, S3 } from 'aws-sdk';
+import type { PromiseResult } from 'aws-sdk/lib/request';
 import * as crypto from 'crypto';
 
 import { ENVIRONMENT } from '@/common/base/enum/common.enum';
@@ -43,6 +44,7 @@ export class FileUploadService implements IFileUploadService {
 
     if (process.env.NODE_ENV === ENVIRONMENT.DEVELOPMENT) {
       this.validateLocalS3();
+      this.createBucketIfNotExists();
     }
   }
 
@@ -59,7 +61,34 @@ export class FileUploadService implements IFileUploadService {
     }
   }
 
-  async checkFileExists(fileHash: string): Promise<boolean> {
+  private async createBucketIfNotExists(): Promise<void> {
+    try {
+      await this.s3.headBucket({ Bucket: this.bucket }).promise();
+    } catch (error) {
+      if (error.code === 'NotFound') {
+        try {
+          await this.s3.createBucket({ Bucket: this.bucket }).promise();
+          this.responseService.log(
+            `Bucket ${this.bucket} created successfully.`,
+          );
+        } catch (createError) {
+          if (createError.code === 'BucketAlreadyOwnedByYou') {
+            this.responseService.log(
+              `Bucket ${this.bucket} already exists and is owned by you.`,
+            );
+          } else {
+            this.responseService.error('Error creating bucket:', createError);
+            throw new Error('Error creating bucket.');
+          }
+        }
+      } else {
+        this.responseService.error('Error checking bucket:', error);
+        throw new Error('Error checking bucket.');
+      }
+    }
+  }
+
+  async checkFileExists(fileHash: string, userId: string): Promise<boolean> {
     const existingFile = await this.s3
       .listObjectsV2({
         Bucket: this.bucket,
@@ -67,19 +96,36 @@ export class FileUploadService implements IFileUploadService {
       })
       .promise();
 
-    return existingFile.Contents && existingFile.Contents.length > 0;
+    if (existingFile.Contents && existingFile.Contents.length > 0) {
+      const key = existingFile.Contents[0].Key;
+      if (key) {
+        const headObject = await this.s3
+          .headObject({
+            Bucket: this.bucket,
+            Key: key,
+          })
+          .promise();
+
+        return headObject.Metadata.userId === userId;
+      }
+    }
+
+    return false;
   }
 
   async uploadFile({
     file,
+    userId,
   }: {
     file: Express.Multer.File;
+    userId: string;
   }): IPromiseResponse<IUploadFileResponse> {
     try {
       const fileHash = crypto
         .createHash('md5')
         .update(file.buffer)
         .digest('hex');
+
       const existingFile = await this.s3
         .listObjectsV2({
           Bucket: this.bucket,
@@ -102,13 +148,22 @@ export class FileUploadService implements IFileUploadService {
         Bucket: this.bucket,
         Key: file.originalname,
         Body: file.buffer,
-        ContentType: 'application/wasm',
+        ContentType: file.mimetype,
+        ContentEncoding: '7bit',
+        Metadata: {
+          userId: userId,
+          originalname: file.originalname,
+          size: file.size.toString(),
+        },
       };
       const { Location } = await this.s3.upload(params).promise();
+      if (!Location) {
+        throw new Error('Failed to upload file');
+      }
       return this.responseService.createResponse({
         type: 'CREATED',
         message: ServiceMessageFileUpload.FILE_UPLOADED_SUCCESSFULLY,
-        payload: { location: Location, key: file.originalname },
+        payload: { key: file.originalname },
       });
     } catch (error) {
       this.responseService.error('Error in uploadFile:', error);
@@ -134,31 +189,73 @@ export class FileUploadService implements IFileUploadService {
     }
   }
 
-  async generatePresignedUrl(key: string): Promise<string> {
+  async generatePresignedUrl(key: string, userId: string): Promise<string> {
+    const metadata = await this.getFileMetadata(key);
+    if (metadata.userId !== userId) {
+      throw new Error('User does not have permission to access this file.');
+    }
+
     const params = {
       Bucket: this.bucket,
       Key: key,
+      Expires: 60 * 60,
     };
 
     const url = await this.s3.getSignedUrlPromise('getObject', params);
     return url;
   }
 
-  async listWasmFiles(): Promise<string[]> {
+  async listWasmFiles() {
     try {
-      const params = {
+      const params: S3.Types.ListObjectsV2Request = {
         Bucket: this.bucket,
       };
 
       const data = await this.s3.listObjectsV2(params).promise();
 
-      const wasmFiles = (data.Contents || []).map((item) => item.Key || '');
-
-      return wasmFiles;
+      return (data.Contents || []).map((item) => item.Key || '');
     } catch (error) {
       throw new Error('Failed to list Wasm files');
     }
   }
+
+  async getFileMetadata(key: string): Promise<{ [key: string]: string }> {
+    try {
+      const params: S3.Types.HeadObjectRequest = {
+        Bucket: this.bucket,
+        Key: key.toString(),
+      };
+      const headObject = await this.s3.headObject(params).promise();
+      if (headObject.Metadata['userid'] || headObject.Metadata['userId']) {
+        return {
+          userId:
+            headObject.Metadata['userid'] || headObject.Metadata['userId'],
+        };
+      }
+    } catch (error) {
+      this.handleError(error);
+    }
+  }
+
+  async downloadFile(
+    key: string,
+    userId: string,
+  ): Promise<PromiseResult<S3.GetObjectOutput, AWSError>> {
+    const metadata = await this.getFileMetadata(key);
+    if (metadata.userId !== userId) {
+      throw new Error('User does not have permission to access this file.');
+    }
+
+    const params: S3.Types.GetObjectRequest = {
+      Bucket: this.bucket,
+      Key: key,
+      Range: null,
+      ResponseContentType: 'application/wasm',
+    };
+
+    return await this.s3.getObject(params).promise();
+  }
+
   private handleError(error: Error): void {
     this.responseService.errorHandler({
       type: 'INTERNAL_SERVER_ERROR',
