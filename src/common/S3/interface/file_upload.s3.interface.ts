3abb6062da1ce287642bfa88abb47815a2852917
nahuelRosas <nahuel.rosas@mi.unc.edu.ar>
2025-01-08 10:04:53 -0300

    Adds userId parameter to file upload service
    Includes userId in the uploadFile method to associate file uploads with specific users. Updates IUploadFileResponse interface by removing the location property

--- src/common/S3/interface/file_upload.s3.interface.ts
+++ src/common/S3/interface/file_upload.s3.interface.ts
@@ -3,15 +3,16 @@ import { IPromiseResponse } from '@/common/response_service/interface/response.i
 export const FILE_UPLOAD_SERVICE = 'FILE_UPLOAD_SERVICE';
 
 export interface IUploadFileResponse {
-  location: string;
   key: string;
 }
 
 export interface IFileUploadService {
   uploadFile({
     file,
+    userId,
   }: {
     file: Express.Multer.File;
+    userId: string;
   }): IPromiseResponse<IUploadFileResponse>;
 
   deleteFile({ key }: { key: string }): IPromiseResponse<boolean>;
