e82994e1af67fa9da25cc2b436f715060cf72c5f
nahuelRosas <nahuel.rosas@mi.unc.edu.ar>
2025-01-15 12:45:00 -0300

    Removes updateUser method from IUserController interface
    Eliminates the updateUser method and related imports from the IUserController interface to streamline the interface and reduce unused code.

--- src/modules/user/application/interfaces/user.controller.interface.ts
+++ src/modules/user/application/interfaces/user.controller.interface.ts
@@ -1,13 +1,5 @@
-import { IPromiseResponse } from '@/common/response_service/interface/response.interface';
-
 import { User } from '../../domain/user.domain';
-import { UpdateUserDto } from '../dto/update_user.dto';
-import { IUpdateUserResponse } from './user.common.interfaces';
 
 export interface IUserController {
-  updateUser(
-    updateUserDto: UpdateUserDto,
-    user: User,
-  ): IPromiseResponse<IUpdateUserResponse>;
   getMe(user: User): Promise<User>;
 }
