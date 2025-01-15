a1de9363d1572f0a52229bbec371f396cbab4a69
nahuelRosas <nahuel.rosas@mi.unc.edu.ar>
2025-01-15 12:44:02 -0300

    Adds Fargate stop guard for task execution
    Introduces a guard to control the stopping of Fargate tasks based on user balance and task execution time. Ensures user balance is updated accordingly to prevent unauthorized task stoppage.

---
+++ src/modules/ephemeralEnvironment/application/guard/fargateStopGuard.guard.ts
@@ -0,0 +1,58 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Inject,
+  Injectable,
+} from '@nestjs/common';
+
+import { IResponse } from '@/common/response_service/interface/response.interface';
+import { REQUEST_USER_KEY } from '@/modules/auth/domain/auth_type.enum';
+import {
+  IUserService,
+  USER_SERVICE,
+} from '@/modules/user/application/interfaces/user.service.interfaces';
+import { User } from '@/modules/user/domain/user.domain';
+
+import { EphemeralEnvironmentService } from '../service/ephemeralEnvironment.service';
+
+@Injectable()
+export class FargateStopGuard implements CanActivate {
+  constructor(
+    @Inject(USER_SERVICE)
+    private readonly userService: IUserService,
+    private readonly ephemeralEnvironmentService: EphemeralEnvironmentService,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const user = request[REQUEST_USER_KEY] as IResponse<User>;
+
+    if (!user) {
+      return false;
+    }
+
+    const taskStatusResponse =
+      await this.ephemeralEnvironmentService.getTaskStatus(user.payload.id);
+    const taskInfo = taskStatusResponse.payload;
+
+    const currentTime = new Date();
+    const elapsedTime =
+      (currentTime.getTime() - new Date(taskInfo.taskStartedAt).getTime()) /
+      60000;
+    const remainingTime = taskInfo.executionInterval - elapsedTime;
+
+    const costPerMinuteResponse = this.userService.getFargateCostPerMinute();
+    const remainingBalance = remainingTime * costPerMinuteResponse;
+
+    try {
+      await this.userService.updateUserBalance(
+        user.payload.id,
+        remainingBalance,
+        true,
+      );
+      return true;
+    } catch (error) {
+      return false;
+    }
+  }
+}
